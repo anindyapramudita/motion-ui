f233202a6a8040a108b398ca94a56c521526db6b
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-06-18 13:05:33 +0200

    feat(input): expose input component

---
+++ lib/components/Input/input.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from "react";
+import styles from "./styles.module.css";
+
+export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  const { className, ...restProps } = props;
+  return <input className={`${className} ${styles.button}`} {...restProps} />;
+};
