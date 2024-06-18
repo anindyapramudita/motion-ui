7bc57b71d436a6e808e033cde87cdfc86cdfb825
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-06-18 12:06:47 +0200

    fix(build): fix build configuration to avoid error

--- lib/components/Button/button.spec.tsx
+++ lib/components/Button/button.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { Button } from "@/main";
+import { Button } from "@/index";
 
 describe("Button", () => {
   test("renders heading", async () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,33 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
-
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({ include: ["lib"] }),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "./lib/index.ts"),
       name: "Motion UI",
-      formats: ["es"],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: [
         "react",
-        "react/jsx-runtime",
+        "react-dom",
+        "tailwindcss",
         /\.spec\.(ts|tsx)$/,
         /\.stories\.(ts|tsx)$/,
       ],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts"],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
       output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
     },
-    copyPublicDir: false,
+    sourcemap: true,
+    emptyOutDir: true,
   },
 });
