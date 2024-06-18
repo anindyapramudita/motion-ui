b85533354adebba630d1aebf2e7e1baea888bbf0
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-06-18 15:23:00 +0200

    fix(vercel): update build command and rollup setting

--- vite.config.ts
+++ vite.config.ts
@@ -26,15 +26,6 @@ export default defineConfig({
           react: "React",
           "react-dom": "ReactDOM",
         },
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
       },
     },
     sourcemap: true,
