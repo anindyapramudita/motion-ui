e2af239580555bf1bb7a3ef54e8420884a1b77f5
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-07-12 10:21:26 +0200

    feat(input): remove input component

--- lib/components/Input/input.tsx
+++
@@ -1,7 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-import styles from './styles.module.css';
-
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  const { className, ...restProps } = props;
-  return <input className={`${className} ${styles.button}`} {...restProps} />;
-};
