d5aed0b3f3d11e30ba83dd812d278a2a2e634445
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-07-12 10:20:55 +0200

    feat(theme): create style tag for theme

---
+++ lib/theme/default-theme.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  primary: {
+    background: '#1876D1',
+    color: '#fff',
+  },
+  secondary: {
+    background: '#F082AC',
+    color: '#fff',
+  },
+  disabled: {
+    background: '#E0E0E0',
+    color: '#A6A6A6',
+  },
+};
---
+++ lib/theme/index.ts
@@ -0,0 +1,1 @@
+export { StyleTag } from './style-tag';
